2ab683110208f62d94b41921fb545361b7334b33
Yevhenii-Tsariuk <care64678@gmail.com>
2024-09-02 16:34:07 +0300

    changes component ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,9 +2,9 @@ import css from "./ImageCard.module.css";
 
 export default function ImageCard({ item, onImageClick }) {
   return (
-    <div className={css.card} onClick={onImageClick}>
+    <div className={css.card} >
       
-        <img className={css.image} src={item.urls.small} alt={item.alt_description} />
+        <img className={css.image} src={item.urls.small} alt={item.alt_description} onClick={onImageClick}/>
       
     </div>
   );
